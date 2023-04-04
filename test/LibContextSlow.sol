bcddbc57baf2b5c30bacb4c758d32ff0b14a0150
thedavidmeister <thedavidmeister@gmail.com>
2023-04-04 20:49:08 +0400

    tests

--- test/LibContextSlow.sol
+++ test/LibContextSlow.sol
@@ -10,6 +10,7 @@ import "../src/IInterpreterCallerV1.sol";
 library LibContextSlow {
     using LibUint256Array for uint256;
     using LibCast for uint256[];
+
     function hashSlow(SignedContext memory signedContext_) internal pure returns (bytes32) {
         bytes32 a_ = LibHashNoAlloc.hashWords(uint256(uint160(signedContext_.signer)).arrayFrom().asBytes32());
         bytes32 b_ = LibHashNoAlloc.hashWords(signedContext_.context.asBytes32());
@@ -17,5 +18,15 @@ library LibContextSlow {
         bytes32 d_ = LibHashNoAlloc.hashBytes(signedContext_.signature);
         bytes32 e_ = LibHashNoAlloc.combineHashes(c_, d_);
         return e_;
+    }
+
+    function hashSlow(SignedContext[] memory signedContext_) internal pure returns (bytes32) {
+        bytes32 hash_ = HASH_NIL;
+
+        for (uint256 i_ = 0; i_ < signedContext_; i_++) {
+            hash_ = LibHashNoAlloc.combineHashes(hash_, hashSlow(signedContext_));
+        }
+
+        return hash_;
     }
 }
