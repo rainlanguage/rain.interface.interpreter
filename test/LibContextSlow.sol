9c875220911af32b26f8fbe39ef32e350d29a2f9
thedavidmeister <thedavidmeister@gmail.com>
2023-04-04 19:21:55 +0400

    wip on slow hash logic

--- test/LibContextSlow.sol
+++ test/LibContextSlow.sol
@@ -7,13 +7,11 @@ import "../src/IInterpreterCallerV1.sol";
 
 library LibContextSlow {
     function hashSlow(SignedContext memory signedContext_) internal pure returns (bytes32) {
-        bytes32[] memory aWords_ = new bytes32[](1);
-        aWords_[0] = bytes32(uint256(uint160(signedContext_.signer)));
-        bytes32 a_ = LibHashNoAlloc.hash(aWords_);
-        bytes32 b_ = LibHashNoAlloc.hash(signedContext_.context);
-        bytes32 c_ = LibHashNoAlloc.hash(a_, b_);
-        bytes32 d_ = LibHashNoAlloc.hash(signedContext_.signature);
-        bytes32 e_ = LibHashNoAlloc.hash(c_, d_);
+        bytes32 a_ = LibHashNoAlloc.hashWords(signedContext_.signer.arrayFrom().asBytes32());
+        bytes32 b_ = LibHashNoAlloc.hashWords(signedContext_.context.asBytes32());
+        bytes32 c_ = LibHashNoAlloc.combineHashes(a_, b_);
+        bytes32 d_ = LibHashNoAlloc.hashBytes(signedContext_.signature);
+        bytes32 e_ = LibHashNoAlloc.combineHashes(c_, d_);
         return e_;
     }
 }
