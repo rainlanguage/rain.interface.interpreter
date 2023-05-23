89256b4d953883a53b17be6c4e45cd2e3a859b06
thedavidmeister <thedavidmeister@gmail.com>
2023-05-23 17:56:40 +0400

    compiling with deployer discoverable

--- src/deployerDiscoverable/LibDeployerDiscoverable.sol
+++ src/deployerDiscoverable/LibDeployerDiscoverable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CAL
 pragma solidity ^0.8.18;
 
-import "rain.interface.interpreter/IExpressionDeployerV1.sol";
+import "src/IExpressionDeployerV1.sol";
 
 library LibDeployerDiscoverable {
     /// Hack so that some deployer will emit an event with the sender as the
